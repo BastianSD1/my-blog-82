9d6d42f3e19db48014083529789e6995889339a2
BastianSD1 <139516022+BastianSD1@users.noreply.github.com>
2023-11-18 11:36:36 -0600

    Add files via upload

---
+++ Plantilla_Alumno_C82.js
@@ -0,0 +1,45 @@
+console.log("My first console project");
+
+            var first_name = "bastian";
+            var last_name = "salazar";
+
+
+ 
+
+
+*/
+            var full_name ="first_name.concat(last_name)"  ;
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+/
+ var add = a + b;
+
+
+Después, imprime la variable para mostrar el valor de la suma.
+
+Escribir: console.log(add);
+            var add = a+b   ;
+            console.log(add );
+
+
+
+
+var sub = a - b;
+
+
+
+
+            var sub =   ;
+            console.log(sub );
+ var multiple = a * b;
+
+Después, imprime la variable para mostrar el valor de la multiplicación.;
+
+ console.log(multiple);
+            var multiple =   ;
+            console.log();
+
+            var divide = a / b ;
+            console.log(divide);
